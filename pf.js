8d8b47103b1d7e556ce5958fc051a2fae2c1075b
ketaki-jadhav-portfolio <69757645+ketaki-jadhav-portfolio@users.noreply.github.com>
2020-08-16 19:37:57 +0530

    Add files via upload

---
+++ pf.js
@@ -0,0 +1,19 @@
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function abcd(div){
+ var x= document.getElementById("html");
+ if(x.style.sidplay==='none'){
+ 	x.style.display='inline';
+ }
+ else{
+ 	x.style.display='none'
+ }
+}
